27d0515bde918746abeec3878f0575cb2b1469d7
Alex Burgett <bogan87@gmail.com>
2017-06-03 11:18:29 +1000

    proj: correcting folder values in publish script

--- auxil/publish.sh
+++ auxil/publish.sh
@@ -2,21 +2,21 @@
 
 echo 'Pusblishing IS4 CLI OSX...'
 pushd $PWD
-cd $PWD/src/AbDev.IdentityServer4.Cli
+cd $PWD/src/AbDevel.IdentityServer4.Cli
 dotnet restore && dotnet publish -c Release -o ./publish/macos --runtime osx.10.12-x64
 popd
 echo 'Pusblished IS4 CLI OSX!'
 
 echo 'Pusblishing IS4 CLI Windows...'
 pushd $PWD
-cd $PWD/src/AbDev.IdentityServer4.Cli
+cd $PWD/src/AbDevel.IdentityServer4.Cli
 dotnet restore && dotnet publish -c Release -o ./publish/win10 --runtime win10-x64
 popd
 echo 'Pusblished IS4 CLI Windows!'
 
 echo 'Pusblishing IS4 CLI Linux(Debian 8)...'
 pushd $PWD
-cd $PWD/src/AbDev.IdentityServer4.Cli
-dotnet restore && dotnet publish -c Release -o ./publish/win10 --runtime debian.8-x64
+cd $PWD/src/AbDevel.IdentityServer4.Cli
+dotnet restore && dotnet publish -c Release -o ./publish/debian --runtime debian.8-x64
 popd
 echo 'Pusblished IS4 CLI Linux(Debian 8)!'
